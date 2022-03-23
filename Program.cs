c0325273cc54efdfe2dee610f97fe6d9c2d1220b
Сергей <seregabur@live.ru>
2022-03-23 19:45:43 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace New_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int money = 32;
+            byte ammo = 1;
+            bool guest_is_completed = true;
+            short hull = 32600;
+            uint stars = 1000000;
+            byte body = 255;
+            sbyte karma = -128;
+            long distance_to_the_moon = 10000000000;
+            ulong distance_to_the_sun = 2000000000000;
+            double distance_to_the_plaer = 10.2;
+        }
+    }
+}
